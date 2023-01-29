dff635e22d0ea8a073fdd2271d4777fc49fb950a
NdekoCode <arickbulakali@gmail.com>
2023-01-29 22:19:09 +0200

    docs: table multidimension

--- variable.php
+++ variable.php
@@ -1,19 +1,23 @@
 <?php
 
-use function PHPSTORM_META\map;
 
 require_once("./setup.php");
 
 $prenom = "Marc";
 $nom = "Doe";
-$notes = [
+$eleves = [
     [
         "nom" => "Marc",
         "prenom" => "Louis",
         "notes" => [1, 12, 15]
+    ],
+    [
+        "nom" => "Jean",
+        "prenom" => "Doe",
+        "notes" => [12, 13, 15]
     ]
 ];
-dump($notes);
+dump($eleves);
 // echo "Bonjour $nom $prenom, vous avez eu $moyenne de moyenne";
 die();
 dump($name);
