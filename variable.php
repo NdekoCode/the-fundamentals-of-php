f326cc08d967eb528366ce6c9587dceb7e5e6cba
NdekoCode <arickbulakali@gmail.com>
2023-01-29 21:22:23 +0200

    docs: init php table

--- variable.php
+++ variable.php
@@ -1,11 +1,19 @@
 <?php
+
+use function PHPSTORM_META\map;
+
 require_once("./setup.php");
 
 $prenom = "Marc";
 $nom = "Doe";
-$note1 = 10;
-$note2 = 20;
-$moyenne = (($note1 + $note2) / 2);
-echo "Bonjour $nom $prenom, vous avez eu $moyenne de moyenne";
+$notes = [
+    [
+        "nom" => "Marc",
+        "prenom" => "Louis",
+        "notes" => [1, 12, 15]
+    ]
+];
+dump($notes);
+// echo "Bonjour $nom $prenom, vous avez eu $moyenne de moyenne";
 die();
 dump($name);
