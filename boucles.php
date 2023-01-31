12bccb18a3a8b779e4c7345d3e8bd4ea3c90e37a
NdekoCode <arickbulakali@gmail.com>
2023-01-31 08:59:26 +0200

    docs: exos The Price is Right with condition

--- boucles.php
+++ boucles.php
@@ -1,10 +1,21 @@
 <?php
 // Tant Que, Pour, Pour chaque
+$nbr_secret = rand(1, 100);
+$score = 3;
 $number = null;
 // Redemander l'utilisateur un nombre tant que le nombre taper est different de 10
-while ($number !== 10) {
-    $number = (int)readline("Entrer un nombre ");
-    if ($number === 10) {
-        print "Bravo, vous avez entrer 10\n";
+while ($number !== 10 && $score > 0) {
+    $number = (int)readline("Entrer un nombre entre 1 et 100 : ");
+    if ($number > 10) {
+        print "Le nombre est trop grand\n";
+    } elseif ($number <= 10) {
+        print "Le nombre est trop petit\n";
     }
+    $score--;
+}
+if ($number === $nbr_secret) {
+
+    print "Bravo, vous avez entrer 10\n";
+} else {
+    print "Vous avez perdus, le nombre secret etait $nbr_secret\n";
 }
