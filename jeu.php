7b73dbec88d67abb76fa78d96880f048c29ef5cd
NdekoCode <arickbulakali@gmail.com>
2023-02-04 19:49:56 +0200

    refactor: separate view and logic

--- jeu.php
+++ jeu.php
@@ -5,22 +5,30 @@ loadFile("header", data: compact('title'));
 
 printIt($_GET);
 dump($_GET);
-$chiffre = isset($_GET['chiffre']) ? (int)htmlentities($_GET['chiffre']) : null;
+$value = null;
+$error = null;
+$success = null;
+if (isset($_GET['chiffre'])) {
+    $value = (int) htmlentities($_GET['chiffre']);
+    if ($value > $aDeviner) {
+        $error = "Votre chiffre est trop grand";
+    } elseif ($value < $aDeviner) {
+        $error = "Votre chiffre est trop petit";
+    } else {
+        $success = "Bravo 👏 vous avez trouver le chiffre $value";
+    }
+}
 ?>
 <h1>Deviner un nombre</h1>
-<?php if ($chiffre) : ?>
-    <?php if ($chiffre > $aDeviner) : ?>
-        <div class="alert alert-danger">Le chiffre est trop grand</div>
-    <?php elseif ($chiffre < $aDeviner) : ?>
-        <div class="alert alert-danger">Le chiffre est trop petit</div>
-    <?php else : ?>
-        <div class="alert alert-success">Bravo vous avez trouver le chiffre</div>
-    <?php endif; ?>
-
+<?php if ($error) : ?>
+    <div class="alert alert-danger"><?= $error ?></div>
+<?php elseif ($success) : ?>
+    <div class="alert alert-success"><?= $success ?></div>
 <?php endif; ?>
+
 <form action="/jeu.php" method="GET" class="d-flex align-items-center">
     <div class="">
-        <input type="number" value="<?= $chiffre ?>" name="chiffre" class="form-control rounded-0 rounded-start">
+        <input type="number" value="<?= $value ?>" name="chiffre" class="form-control rounded-0 rounded-start">
     </div>
     <button class="rounded btn btn-primary rounded-0 rounded-end">Deviner</button>
 </form>
